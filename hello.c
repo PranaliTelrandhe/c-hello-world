5eb581471c252b8b3e59bf3cf5112e686420491e
PranaliTelrandhe <pranalitelrandhe252@gmail.com>
2026-02-02 20:21:46 +0100

    Changes: Taking default data in source code

--- hello.c
+++ hello.c
@@ -3,29 +3,24 @@
 // Function to calculate factorial
 int factorial(int n) {
     int result = 1;
-    for (int i = 2; i <= n; i++) {
+    for (int i = 2; i <= n; i++)
         result *= i;
-    }
     return result;
 }
 
 // Function to sum numbers from 1 to n
 int sum_up_to(int n) {
     int sum = 0;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++)
         sum += i;
-    }
     return sum;
 }
 
 int main() {
-    int number;
+    // Use a default number instead of interactive input
+    int number = 5;
 
-    printf("Enter a positive integer: ");
-    if (scanf("%d", &number) != 1 || number < 0) {
-        printf("Invalid input. Please enter a positive integer.\n");
-        return 1;
-    }
+    printf("Using default number: %d\n", number);
 
     int fact = factorial(number);
     int sum = sum_up_to(number);
@@ -34,9 +29,8 @@ int main() {
     printf("Sum of numbers from 1 to %d is: %d\n", number, sum);
 
     printf("Counting down from %d to 1:\n", number);
-    for (int i = number; i >= 1; i--) {
+    for (int i = number; i >= 1; i--)
         printf("%d ", i);
-    }
     printf("\n");
 
     return 0;
