b029e05a97589d7f2549feaf15e5a0a021e38aba
PranaliTelrandhe <pranalitelrandhe252@gmail.com>
2026-02-02 20:14:17 +0100

    Changed the code

--- hello.c
+++ hello.c
@@ -1,6 +1,43 @@
-#include <stdio.h>  // for printf
+#include <stdio.h>
+
+// Function to calculate factorial
+int factorial(int n) {
+    int result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+// Function to sum numbers from 1 to n
+int sum_up_to(int n) {
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
+}
 
 int main() {
-    printf("hello, world\n");
-    return 0;      // main should return an int
+    int number;
+
+    printf("Enter a positive integer: ");
+    if (scanf("%d", &number) != 1 || number < 0) {
+        printf("Invalid input. Please enter a positive integer.\n");
+        return 1;
+    }
+
+    int fact = factorial(number);
+    int sum = sum_up_to(number);
+
+    printf("Factorial of %d is: %d\n", number, fact);
+    printf("Sum of numbers from 1 to %d is: %d\n", number, sum);
+
+    printf("Counting down from %d to 1:\n", number);
+    for (int i = number; i >= 1; i--) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    return 0;
 }
